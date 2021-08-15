50a27c2621dff5203b5ac017bea7b0f155290a67
Andrey Patrakov <65234042+sameman666@users.noreply.github.com>
2021-08-15 15:57:52 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll(`.task__accordion-title`).forEach((item) => {
-  item.addEventListener(`click`, () => {
-    item.parentNode.classList.toggle(`task__accordion--opened`)
-  })
-});
